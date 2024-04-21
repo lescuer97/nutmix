2173366be48d0dc69e652ce8de1b1d0151bb6229
leonardo <leoescuer@protonmail.com>
2024-04-21 20:26:18 +0200

    small and mint request

--- mint/cashu/types.go
+++ mint/cashu/types.go
@@ -1,10 +1,24 @@
 package cashu
 
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+
+	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/lescuer97/cashu-v4v/crypto"
+	"github.com/tyler-smith/go-bip32"
+)
+
 
 type BlindedMessage struct {
 	Amount int32  `json:"amount"`
 	Id     string `json:"id"`
 	B_     string `json:"B_"`
+}
+
+func (b *BlindedMessage) SignBlindedMessage() {
+
 }
 
 type BlindSignature struct {
@@ -13,11 +27,39 @@ type BlindSignature struct {
 	C_     string `json:"C_"`
 }
 
+func GenerateBlindSignature (privateKey *bip32.Key, blindedMessage BlindedMessage) (BlindSignature, error) {
+
+			k := secp256k1.PrivKeyFromBytes(privateKey.Key)
+
+			decodedBlindFactor, err := hex.DecodeString(blindedMessage.B_)
+
+			if err != nil {
+				log.Println(fmt.Errorf("DecodeString: %v", err))
+                return BlindSignature{}, err
+			}
+
+			B_, err := secp256k1.ParsePubKey(decodedBlindFactor)
+
+			if err != nil {
+				log.Println(fmt.Errorf("ParsePubKey: %v", err))
+                return BlindSignature{}, err
+			}
+
+			C_ := crypto.SignBlindedMessage(B_, k)
+
+            return BlindSignature {
+				Amount: blindedMessage.Amount,
+				Id:     blindedMessage.Id,
+				C_:     hex.EncodeToString(C_.SerializeCompressed()),
+			}, nil
+}
+
+
 type Proof struct {
 	Amount int32  `json:"amount"`
 	Id     string `json:"id"`
 	Secret string `json:"secret"`
-	C_     string `json:"C_"`
+	C     string `json:"C"`
 }
 
 type MintError struct {
@@ -30,8 +72,20 @@ type Keyset struct {
 	Active    bool   `json:"active" db:"active"`
 	Unit      string `json:"unit"`
 	Amount    int    `json:"amount"`
-	PubKey    []byte `json:"pub_key"`
+	PrivKey  string `json:"priv_key"`
 	CreatedAt int64  `json:"created_at"`
+}
+
+func (keyset *Keyset) GetPubKey() ([]byte, error)  {
+    privKey, err := bip32.B58Deserialize(keyset.PrivKey)
+
+    if err != nil {
+        return nil, err
+
+    }
+    return privKey.PublicKey().Key, err
+
+
 }
 
 type Seed struct {
@@ -42,3 +96,59 @@ type Seed struct {
 	Id        string
 }
 
+type SwapMintMethod struct {
+	Method    string `json:"method"`
+	Unit      string `json:"unit"`
+	MinAmount int    `json:"min_amount"`
+	MaxAmount int    `json:"max_amount"`
+}
+
+type SwapMintInfo struct {
+	Methods  *[]SwapMintMethod `json:"methods,omitempty"`
+	Disabled bool              `json:"disabled"`
+}
+
+type GetInfoResponse struct {
+	Name            string     `json:"name"`
+	Version         string     `json:"version"`
+	Pubkey          string     `json:"pubkey"`
+	Description     string     `json:"description"`
+	DescriptionLong string     `json:"description_long"`
+	Contact         [][]string `json:"contact"`
+	Motd            string     `json:"motd"`
+	Nuts            map[string]SwapMintInfo
+}
+
+
+type KeysetResponse struct {
+	Id   string            `json:"id"`
+	Unit string            `json:"unit"`
+	Keys map[string]string `json:"keys"`
+}
+
+type PostMintQuoteBolt11Request struct {
+	Amount int64  `json:"amount"`
+	Unit   string `json:"unit"`
+}
+
+type PostMintQuoteBolt11Response struct {
+	Quote string  `json:"quote"`
+	Request   string `json:"request"`
+	Paid   bool `json:"paid"`
+	Expiry   uint64 `json:"expiry"`
+}
+
+type PostMintBolt11Request struct {
+    Quote string `json:"quote"`
+    Outputs []BlindedMessage `json:"outputs"`
+}
+
+type PostMintBolt11Response struct {
+    Signatures []BlindSignature `json:"signatures"`
+}
+
+type BasicKeysetResponse struct {
+	Id     string `json:"id"`
+	Unit   string `json:"unit"`
+	Active bool   `json:"active"`
+}
--- mint/db.go
+++ mint/db.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+
 	"github.com/jackc/pgx/v5"
 	"github.com/lescuer97/cashu-v4v/cashu"
 )
 
-func GetAllSeeds(conn *pgx.Conn) []cashu.Seed {
+func GetAllSeeds(conn *pgx.Conn) ([]cashu.Seed, error) {
 	var seeds []cashu.Seed
 
 	rows, err := conn.Query(context.Background(), "SELECT * FROM seeds")
 
 	if err != nil {
 		if err == pgx.ErrNoRows {
-			return seeds
+			return seeds, fmt.Errorf("No rows found: %v", err)
 		}
 		log.Fatal("Error checking for  seeds: ", err)
 	}
@@ -24,15 +26,15 @@ func GetAllSeeds(conn *pgx.Conn) []cashu.Seed {
 	keysets_collect, err := pgx.CollectRows(rows, pgx.RowToStructByName[cashu.Seed])
 
 	if err != nil {
-		log.Fatal("Error checking for seeds: ", err)
+		return keysets_collect, fmt.Errorf("Collecting rows: %v", err)
 	}
 
-	return keysets_collect
+	return keysets_collect, nil
 }
 
 func GetActiveSeed(conn *pgx.Conn) (cashu.Seed, error) {
 
-    var err error
+	var err error
 	rows, err := conn.Query(context.Background(), "SELECT * FROM seeds WHERE active")
 	if err != nil {
 		log.Fatal("Error checking for active keyset: ", err)
@@ -43,19 +45,19 @@ func GetActiveSeed(conn *pgx.Conn) (cashu.Seed, error) {
 
 	if err != nil {
 		log.Fatal("Error checking for active keyset: ", err)
-        return seed, err
+		return seed, err
 	}
 
 	return seed, nil
 }
 
-func CheckForActiveKeyset(conn *pgx.Conn) []cashu.Keyset {
+func CheckForActiveKeyset(conn *pgx.Conn) ([]cashu.Keyset, error) {
 	var keysets []cashu.Keyset
 
 	rows, err := conn.Query(context.Background(), "SELECT * FROM keysets WHERE active")
 	if err != nil {
 		if err == pgx.ErrNoRows {
-			return keysets
+			return keysets, fmt.Errorf("No rows found: %v", err)
 		}
 		log.Fatal("Error checking for active keyset: ", err)
 	}
@@ -64,19 +66,87 @@ func CheckForActiveKeyset(conn *pgx.Conn) []cashu.Keyset {
 	keysets_collect, err := pgx.CollectRows(rows, pgx.RowToStructByName[cashu.Keyset])
 
 	if err != nil {
-		log.Fatal("Error checking for active keyset: ", err)
+
+		return keysets_collect, fmt.Errorf("Collecting rows: %v", err)
 	}
 
-	return keysets_collect
+	return keysets_collect, nil
 }
 
-func CheckForKeysetById(conn *pgx.Conn, id string) []cashu.Keyset {
+func CheckForKeysetById(conn *pgx.Conn, id string) ([]cashu.Keyset, error) {
 	var keysets []cashu.Keyset
 
 	rows, err := conn.Query(context.Background(), "SELECT * FROM keysets WHERE id = $1", id)
 	if err != nil {
 		if err == pgx.ErrNoRows {
-			return keysets
+			return keysets, fmt.Errorf("No rows found: %v", err)
+		}
+	}
+	defer rows.Close()
+
+	keysets_collect, err := pgx.CollectRows(rows, pgx.RowToStructByName[cashu.Keyset])
+
+	if err != nil {
+		return keysets_collect, fmt.Errorf("Collecting rows: %v", err)
+	}
+
+	return keysets_collect, nil
+}
+
+func SaveNewSeed(conn *pgx.Conn, seed *cashu.Seed) error {
+	_, err := conn.Exec(context.Background(), "INSERT INTO seeds (seed, active, created_at, unit, id) VALUES ($1, $2, $3, $4, $5)", seed.Seed, seed.Active, seed.CreatedAt, seed.Unit, seed.Id)
+	if err != nil {
+		log.Fatal("Error saving new seed: ", err)
+		return fmt.Errorf("inserting to DB: %v", err)
+	}
+	return nil
+}
+
+func SaveNewKeysets(conn *pgx.Conn, keyset []cashu.Keyset) error {
+	for _, key := range keyset {
+		_, err := conn.Exec(context.Background(), "INSERT INTO keysets (id, active, unit, amount, privkey, created_at) VALUES ($1, $2, $3, $4, $5, $6)", key.Id, key.Active, key.Unit, key.Amount, key.PrivKey, key.CreatedAt)
+		if err != nil {
+			return fmt.Errorf("Inserting to keysets: %v", err)
+		}
+	}
+	return nil
+}
+
+func SaveQuoteRequest(conn *pgx.Conn, request cashu.PostMintQuoteBolt11Response) error {
+	_, err := conn.Exec(context.Background(), "INSERT INTO mint_request (quote, request, paid, expiry) VALUES ($1, $2, $3, $4)", request.Quote, request.Request, request.Paid, request.Expiry)
+	if err != nil {
+		return fmt.Errorf("Inserting to mint_request: %v", err)
+
+	}
+	return nil
+}
+
+func GetQuoteById(conn *pgx.Conn, id string) (cashu.PostMintQuoteBolt11Response, error) {
+
+	rows, err := conn.Query(context.Background(), "SELECT * FROM mint_request WHERE quote = $1", id)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return cashu.PostMintQuoteBolt11Response{}, fmt.Errorf("No rows found: %v", err)
+		}
+	}
+	defer rows.Close()
+
+	quote, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[cashu.PostMintQuoteBolt11Response])
+
+	if err != nil {
+		return quote, fmt.Errorf("CollectOneRow: %v", err)
+	}
+
+	return quote, nil
+}
+
+func GetKeysetsByAmountList(conn *pgx.Conn, keyAmounts []int32) (map[int]cashu.Keyset, error) {
+    var keysetMap = make(map[int]cashu.Keyset)
+
+	rows, err := conn.Query(context.Background(), "SELECT * FROM keysets WHERE amount = ANY($1)", keyAmounts)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return keysetMap, fmt.Errorf("No rows found: %v", err)
 		}
 		log.Fatal("Error checking for active keyset: ", err)
 	}
@@ -85,24 +155,13 @@ func CheckForKeysetById(conn *pgx.Conn, id string) []cashu.Keyset {
 	keysets_collect, err := pgx.CollectRows(rows, pgx.RowToStructByName[cashu.Keyset])
 
 	if err != nil {
-		log.Fatal("Error checking for active keyset: ", err)
+
+		return keysetMap, fmt.Errorf("Collecting rows: %v", err)
 	}
 
-	return keysets_collect
-}
+    for _, keyset := range keysets_collect {
+        keysetMap[keyset.Amount] = keyset
+    }
 
-func SaveNewSeed(conn *pgx.Conn, seed *cashu.Seed) {
-	_, err := conn.Exec(context.Background(), "INSERT INTO seeds (seed, active, created_at, unit, id) VALUES ($1, $2, $3, $4, $5)", seed.Seed, seed.Active, seed.CreatedAt, seed.Unit, seed.Id)
-	if err != nil {
-		log.Fatal("Error saving new seed: ", err)
-	}
-}
-
-func SaveNewKeysets(conn *pgx.Conn, keyset []cashu.Keyset) {
-	for _, key := range keyset {
-		_, err := conn.Exec(context.Background(), "INSERT INTO keysets (id, active, unit, amount, pubkey, created_at) VALUES ($1, $2, $3, $4, $5, $6)", key.Id, key.Active, key.Unit, key.Amount, key.PubKey, key.CreatedAt)
-		if err != nil {
-			log.Fatal("Error saving new keyset: ", err)
-		}
-	}
+	return keysetMap, nil
 }
