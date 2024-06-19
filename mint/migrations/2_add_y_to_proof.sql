1fdcc3c6dad7ac10bd05a51ba68366a383d2635b
leonardo <leoescuer@protonmail.com>
2024-06-19 15:08:08 +0200

    remove mint subfolder

--- mint/migrations/2_add_y_to_proof.sql
+++
@@ -1,7 +0,0 @@
-
--- +goose Up
-ALTER TABLE proofs ADD Y TEXT;
-
-
--- +goose Down
-ALTER TABLE proofs DROP COLUMN Y
--- mint/migrations/3_add_version_to_seeds.sql
+++
@@ -1,6 +0,0 @@
--- +goose Up
-ALTER TABLE seeds ADD version INT;
-
-
--- +goose Down
-ALTER TABLE seeds DROP COLUMN version
