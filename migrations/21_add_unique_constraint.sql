844648e70c9c4b0d19a8caf9c01d1f7df3310db5
leonardo <leoescuer@protonmail.com>
2025-02-12 00:45:29 +0100

    Revert "modify just block secret"
    This reverts commit dcf645b2bb3f3d3726f37be6c869133a6b1ccb96.

--- migrations/21_add_unique_constraint.sql
+++ migrations/21_add_unique_constraint.sql
@@ -1,10 +1,10 @@
 -- +goose Up
-ALTER TABLE proofs ADD CONSTRAINT unique_secret UNIQUE (secret);
+ALTER TABLE proofs ADD CONSTRAINT unique_y UNIQUE (secret, y);
 ALTER TABLE recovery_signature ADD CONSTRAINT unique_recovery_B_ UNIQUE ("B_");
 
 
 
 -- +goose Down
-ALTER TABLE proofs DROP CONSTRAINT unique_secret;
+ALTER TABLE proofs DROP CONSTRAINT unique_y;
 ALTER TABLE recovery_signature DROP CONSTRAINT unique_recovery_B_;
 
