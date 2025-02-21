2412ce8586ee75f1362ff30e50c4b529e628be91
leonardo <leoescuer@protonmail.com>
2025-02-21 13:14:32 +0100

    fix test

--- internal/mint/mint_test.go
+++ internal/mint/mint_test.go
@@ -2,78 +2,19 @@ package mint
 
 import (
 	"context"
-	"encoding/hex"
 	"fmt"
-	"os"
-	"testing"
-	"time"
-
-	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/lescuer97/nutmix/api/cashu"
 	pq "github.com/lescuer97/nutmix/internal/database/postgresql"
 	"github.com/lescuer97/nutmix/internal/lightning"
+	localsigner "github.com/lescuer97/nutmix/internal/signer/local_signer"
 	"github.com/lescuer97/nutmix/internal/utils"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
+	"os"
+	"testing"
+	"time"
 )
-
-const MintPrivateKey string = "0000000000000000000000000000000000000000000000000000000000000001"
-
-func TestCreateNewSeed(t *testing.T) {
-	decodedPrivKey, err := hex.DecodeString(MintPrivateKey)
-	if err != nil {
-		t.Fatal("hex.DecodeString(mint_privkey)")
-	}
-
-	parsedPrivateKey := secp256k1.PrivKeyFromBytes(decodedPrivKey)
-	masterKey, err := MintPrivateKeyToBip32(parsedPrivateKey)
-	if err != nil {
-		t.Fatal("mint.MintPrivateKeyToBip32(parsedPrivateKey)")
-	}
-
-	seed1, err := CreateNewSeed(masterKey, 1, 0)
-	if err != nil {
-		t.Fatal("CreateNewSeed(masterKey, 1, 0)")
-	}
-
-	if seed1.Id != "00bfa73302d12ffd" {
-		t.Errorf("seed id incorrect. %v", seed1.Id)
-
-	}
-
-}
-func TestGeneratedKeysetsMakeTheCorrectIds(t *testing.T) {
-	decodedPrivKey, err := hex.DecodeString(MintPrivateKey)
-	if err != nil {
-		t.Fatal("hex.DecodeString(mint_privkey)")
-	}
-
-	parsedPrivateKey := secp256k1.PrivKeyFromBytes(decodedPrivKey)
-	masterKey, err := MintPrivateKeyToBip32(parsedPrivateKey)
-	if err != nil {
-		t.Fatal("mint.MintPrivateKeyToBip32(parsedPrivateKey)")
-	}
-	seed1, err := CreateNewSeed(masterKey, 1, 0)
-	if err != nil {
-		t.Fatal("CreateNewSeed(masterKey, 1, 0)")
-	}
-
-	keyset, err := DeriveKeyset(masterKey, seed1)
-	if err != nil {
-		t.Fatal("DeriveKeyset(masterKey,seed1 )")
-	}
-	newSeedId, err := cashu.DeriveKeysetId(keyset)
-	if err != nil {
-		t.Fatal("cashu.DeriveKeysetId(keyset)")
-	}
-
-	if newSeedId != "00bfa73302d12ffd" {
-		t.Errorf("seed id incorrect. %v", seed1.Id)
-
-	}
-
-}
 
 func SetupMintWithLightningMockPostgres(t *testing.T) *Mint {
 	const posgrespassword = "password"
@@ -106,38 +47,9 @@ func SetupMintWithLightningMockPostgres(t *testing.T) *Mint {
 		t.Fatal("Error conecting to db", err)
 	}
 
-	seeds, err := db.GetAllSeeds()
-
+	signer, err := localsigner.SetupLocalSigner(db)
 	if err != nil {
-		t.Fatalf("Could not keysets: %v", err)
-	}
-
-	decodedPrivKey, err := hex.DecodeString(MintPrivateKey)
-	if err != nil {
-		t.Fatalf("hex.DecodeString(mint_privkey): %+v ", err)
-	}
-
-	parsedPrivateKey := secp256k1.PrivKeyFromBytes(decodedPrivKey)
-	masterKey, err := MintPrivateKeyToBip32(parsedPrivateKey)
-	if err != nil {
-		t.Fatalf("mint.MintPrivateKeyToBip32(parsedPrivateKey): %+v ", err)
-	}
-	// incase there are no seeds in the db we create a new one
-	if len(seeds) == 0 {
-
-		seed, err := CreateNewSeed(masterKey, 1, 0)
-		if err != nil {
-			t.Fatalf("mint.CreateNewSeed(masterKey, 1, 0) %+v ", err)
-		}
-
-		err = db.SaveNewSeeds([]cashu.Seed{seed})
-
-		seeds = append(seeds, seed)
-
-		if err != nil {
-			t.Fatalf("SaveNewSeed: %+v ", err)
-		}
-
+		t.Fatalf("localsigner.SetupLocalSigner(db): %v", err)
 	}
 
 	config, err := SetUpConfigDB(db)
@@ -155,7 +67,7 @@ func SetupMintWithLightningMockPostgres(t *testing.T) *Mint {
 		t.Fatalf("could not setup config file: %+v ", err)
 	}
 
-	mint, err := SetUpMint(ctx, parsedPrivateKey, seeds, config, db)
+	mint, err := SetUpMint(ctx, config, db, &signer)
 
 	if err != nil {
 		t.Fatalf("SetUpMint: %+v ", err)
