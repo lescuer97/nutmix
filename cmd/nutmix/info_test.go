c6f9749c97ce99433ff59324c55d084a13b420ce
Leonardo Escuer <47948594+lescuer97@users.noreply.github.com>
2025-04-27 16:55:28 +0200

    GitHub build (#147)
    * github pipeline releasing
    
    * change version
    
    * correct build
    
    * fix pipeline
    
    * fix build
    
    * update upload

--- cmd/nutmix/info_test.go
+++ cmd/nutmix/info_test.go
@@ -71,7 +71,7 @@ func TestMintInfo(t *testing.T) {
 		t.Errorf("Error unmarshalling response: %v", err)
 	}
 
-	if mintInfo.Version != "NutMix/0.3.0" {
+	if mintInfo.Version != "NutMix/0.3.1" {
 		t.Errorf("Incorrect version  %v", mintInfo.Version)
 	}
 
